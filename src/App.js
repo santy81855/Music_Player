de398525500aa4fdb115b03f86517bc889cc922f
santy81855 <mynameissantiagogarcia@gmail.com>
2022-03-25 10:22:43 -0400

    changed main text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          OUR AMAZING MUSIC PLAYER.
         </p>
         <a
           className="App-link"
