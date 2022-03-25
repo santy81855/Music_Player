a03c5595027126b46fa9fb7f5d285fd0c1cedfc1
paden-myjak <55555791+paden-myjak@users.noreply.github.com>
2022-03-25 19:37:55 -0400

    update and thigns

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Update by: Paden Myjak
         </a>
       </header>
     </div>
