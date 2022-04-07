036ebbce7bcb130089732e1d26de2534b31c9163
Nestor Rubio <nestordrubio9@gmail.com>
2022-04-06 23:08:16 -0400

    testing formatting of data and ensuring we can get cover art too...we can

--- src/components/PlayBar.js
+++ src/components/PlayBar.js
@@ -2,14 +2,26 @@ import './PlayBar.css';
 import React from 'react';
 import AudioPlayer from 'react-h5-audio-player';
 import 'react-h5-audio-player/lib/styles.css';
+import SongData from '../databases/songs/songs.json';
+
 class PlayBar extends React.Component {
+  constructor(props){
+    super(props);
+  }
   render() {
+    // testing
+    var target = "Ukelele";
+    var songs = SongData.filter(function(song){
+      return song.title == target
+    })
+    var song = songs[0]
+    console.log(song)
     return (
       <div className="PlayBar">
-        
+      <img src={song.cover_art}/>
       <AudioPlayer
       autoPlay
-      src="https://www.bensound.com/bensound-music/bensound-november.mp3"
+      src={song.mp3address} //https://www.bensound.com/bensound-music/bensound-november.mp3
       onPlay={e => console.log("onPlay")}
       onPause={e=>console.log("pause")}
       // other props here
