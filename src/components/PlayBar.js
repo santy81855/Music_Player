f66816c285fe15f545a6a0e38a05d89a5203ad64
Nestor Rubio <nestordrubio9@gmail.com>
2022-04-14 21:49:06 -0400

    skip functionality with login stuff figured out too

--- src/components/PlayBar.js
+++ src/components/PlayBar.js
@@ -29,6 +29,7 @@ function PlayBar(props){
     };
   
     const handleClickNext = () => {
+      console.log("nextclick")
       setTrackIndex((currentTrack) =>
         currentTrack < props.playlist.songs.length - 1 ? currentTrack + 1 : 0
       );
@@ -40,15 +41,19 @@ function PlayBar(props){
         {/* <img src={SongData[props.playlist.songs[trackIndex]].cover_art}/> */}
         <AudioPlayer
           style={PlayBarStyle}
+          showSkipControls={true}
+          showJumpControls={false}
           autoPlay={false}
           volume={0.5}
           src={SongData[props.playlist.songs[trackIndex]].mp3address}
           header={`Now Playing: "${SongData[props.playlist.songs[trackIndex]].title}" by ${SongData[props.playlist.songs[trackIndex]].artist}`}
           onPlay={e => console.log("onPlay")}
           onPause={e=>console.log("pause")}
-          onClickPrevious={handleClickPrevious}
+          onClickPrevious={console.log("prev")}
           onClickNext={handleClickNext}
           onEnded={handleClickNext}
+          onClickForward={handleClickNext}
+          progressJumpSteps={{backward:1000000,forward:1000000}}
         />
       </div>
     );
