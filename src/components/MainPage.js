36f9268be6a1c415227dd00fa99696cd9428b50b
paden-myjak <55555791+paden-myjak@users.noreply.github.com>
2022-04-17 18:17:57 -0400

    MADE MUSIC PLAY FROM SEARCH!!! (Loses playlist for now but that's a tomorrow problem!)

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,5 +1,5 @@
 import './MainPage.css';
-import React from 'react';
+import React, {createContext} from 'react';
 import Library from '../pages/Library.js';
 import Settings from '../pages/Settings.js';
 import SearchSong from '../pages/SearchSong.js';
@@ -8,23 +8,41 @@ import SearchArtist from '../pages/SearchArtist.js';
 import PlayBar from './PlayBar';
 import Playlists from '../databases/songs/playlists.json';
 
-const pages = [<Settings/>, <Library/>, <SearchSong/>, <SearchPlaylist/>, <SearchArtist/>];
+// const MyContext = createContext("");
+
+// const pagesTest = [Settings, Library, SearchSong, SearchPlaylist, SearchArtist ];
 
 class MainPage extends React.Component {
   // constructor(props) {
   //   super(props);
   //   this.index = 1;
-  // }  
+  // }
+  
+  state = {
+    Song: null,
+  }
+  
+  handleCallback = (childSongData) => {
+    this.setState({Song: childSongData})
+    console.log("In Main:", childSongData.title)
+  }
+  
+  pages = [ <Settings />,
+            <Library/>,
+            <SearchSong value={"Hello from Main!"} callback={this.handleCallback}/>,
+            <SearchPlaylist/>,
+            <SearchArtist/>
+          ];
   
   render() {
     return (
 
       <div className = "App-MainPage-and-PlayBar">
         <div className="MainPage">
-          {pages[this.props.selected]}
+          {this.pages[this.props.selected]}
         </div>
 
-        <PlayBar playlist={Playlists[0]} props={this.props}/>
+        <PlayBar song={this.state.Song} playlist={Playlists[0]} props={this.props}/>
       </div>
     );
   }
--- src/components/SearchResult.js
+++ src/components/SearchResult.js
@@ -1,11 +1,29 @@
 import './SearchResult.css';
-import React from 'react';
+import React, {createContext} from 'react';
+// import MyContext from '../components/MainPage.js'
 
+const MyContext = createContext("");
 
 class SearchResult extends React.Component {
+  constructor(props) {
+    super(props);
+    this.props = props;
+  }
+  
+  onTrigger = () => {
+    // console.log(this.props.value);
+    // console.log("In Search Results");
+    this.props.callback(this.props.Song);
+  };
+  
   render() {
     return (
-      <div className = "SearchResult" onClick={() => console.log("Clicked:", this.props.Song.title)} style = {{cursor: 'pointer'}}>
+      // <div className = "SearchResult" onClick={() => console.log("Clicked:", this.props.Song.title)} style = {{cursor: 'pointer'}}>
+      <div className = "SearchResult"
+        onClick={this.onTrigger}
+        style = {{cursor: 'pointer'}}
+        // value={"Hello Context!"}
+      >
         <div className = "LeftBox">
           <img src={this.props.Song.cover_art} alt="png" width="100%" height="100%" />
         </div>
