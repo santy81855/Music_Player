0a0c5b326cd2795dce01cba265cfd62e81314cbd
paden-myjak <55555791+paden-myjak@users.noreply.github.com>
2022-04-17 18:40:44 -0400

    Minor CSS changes (Totally didnt bring down production)

--- src/components/PlayBar.js
+++ src/components/PlayBar.js
@@ -1,10 +1,10 @@
 import './PlayBar.css';
 import React, {useState} from 'react';
-import PlayBarStyle from './PlayBarStyle.scss';
-import SongData from '../databases/songs/songs.json';
-
 import AudioPlayer from 'react-h5-audio-player';
 import 'react-h5-audio-player/lib/styles.css';
+
+import SongData from '../databases/songs/songs.json';
+import PlayBarStyle from './PlayBarStyle.scss';
 
 
 function PlayBar(props) { 
@@ -48,16 +48,10 @@ function PlayBar(props) {
           style={PlayBarStyle}
           autoPlay={false}
           volume={0.5}
-          
-          // src={SongData[props.playlist.songs[trackIndex]].mp3address}
-          // header={`Now Playing: "${SongData[props.playlist.songs[trackIndex]].title}" by ${SongData[props.playlist.songs[trackIndex]].artist}`}
-      
           src={SongData[props.playlist.songs[trackIndex]].mp3address}
           header={`Now Playing: "${SongData[props.playlist.songs[trackIndex]].title}" by ${SongData[props.playlist.songs[trackIndex]].artist}`}
-          
           onPlay={() => console.log("onPlay:", SongData[props.playlist.songs[trackIndex]].title)}
           onPause={() =>console.log("onPause:", SongData[props.playlist.songs[trackIndex]].mp3address)}
-
           onClickPrevious={handleClickPrevious}
           onClickNext={handleClickNext}
           onEnded={handleClickNext}
@@ -68,18 +62,3 @@ function PlayBar(props) {
 }
 
 export default PlayBar;
-
-// class PlayBar extends React.Component {
-//   constructor(props){
-//     super(props);
-//   }
-  //render() {
-    /*// testing
-    var target = "Ukelele";
-    var songs = SongData.filter(function(song){
-      return song.title == target
-    })
-    // songs is an array of search results
-    var song = songs[0]
-    // song is current song as it was top result
-    console.log(song)*/
