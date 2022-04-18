710079ff25ffeb2d2cb6b283be73cf56245315e6
paden-myjak <55555791+paden-myjak@users.noreply.github.com>
2022-04-18 08:46:23 -0400

    Beginning implementation of Library.js

--- src/pages/Library.js
+++ src/pages/Library.js
@@ -1,14 +1,60 @@
 import React, { Component } from 'react';
 import './Library.css';
 
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+
+import settingsPNG from '../icons/settings.png';
+
+
+const responsive = {
+  superLargeDesktop: {
+    // the naming can be any, depends on you.
+    breakpoint: { max: 4000, min: 3000 },
+    items: 5
+  },
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 3
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 464 },
+    items: 2
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 1
+  }
+};
+
 class Library extends Component {
   render() {
     return (
       <div className="Library">
-        {/* MainPage */}
-        Library
+        <div className="LibraryWrapper">
+          {/* Library */}   
+          <div className="LibraryDivider"/>
 
-        
+          <div className="LibraryGallery">
+            <div className="LibraryGalleryButton"/>
+            <div className="LibraryGalleryCenter">
+              Songs
+            </div>
+            <div className="LibraryGalleryButton"/>
+          </div>
+
+          <div className="LibraryDivider"/>
+
+          <div className="LibraryGallery">
+            <div className="LibraryGalleryButton"/>
+            <div className="LibraryGalleryCenter">
+              Playlists
+            </div>
+            <div className="LibraryGalleryButton"/>
+          </div>
+
+          <div className="LibraryDivider"/>
+        </div>
       </div>
     );
   }
