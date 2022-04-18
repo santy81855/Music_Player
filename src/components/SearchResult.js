d0c93c69aec2bee660e5ae2344040ec103194344
paden-myjak <55555791+paden-myjak@users.noreply.github.com>
2022-04-18 08:00:48 -0400

    Cleaning up before implementing Library

--- src/components/SearchResult.js
+++ src/components/SearchResult.js
@@ -1,8 +1,6 @@
 import './SearchResult.css';
-import React, {createContext} from 'react';
-// import MyContext from '../components/MainPage.js'
+import React from 'react';
 
-const MyContext = createContext("");
 
 class SearchResult extends React.Component {
   constructor(props) {
@@ -11,18 +9,14 @@ class SearchResult extends React.Component {
   }
   
   onTrigger = () => {
-    // console.log(this.props.value);
-    // console.log("In Search Results");
     this.props.callback(this.props.Song);
   };
   
   render() {
     return (
-      // <div className = "SearchResult" onClick={() => console.log("Clicked:", this.props.Song.title)} style = {{cursor: 'pointer'}}>
       <div className = "SearchResult"
         onClick={this.onTrigger}
         style = {{cursor: 'pointer'}}
-        // value={"Hello Context!"}
       >
         <div className = "LeftBox">
           <img src={this.props.Song.cover_art} alt="png" width="100%" height="100%" />
