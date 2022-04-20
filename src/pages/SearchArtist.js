5a88aab7cb2502e0075cdd98957a75fe5c084d45
Kooven47 <kevfonso@gmail.com>
2022-04-19 23:47:16 -0400

    implemented rough filtering (needs CSS) for searches, changed electronica genre to genre in songs.json

--- src/pages/SearchArtist.js
+++ src/pages/SearchArtist.js
@@ -2,13 +2,40 @@ import React, { useState} from 'react';
 import './SearchArtist.css';
 import songData from '../databases/songs/songs.json';
 import SearchResult from '../components/SearchResult.js';
+import FilterPanel from '../components/FilterPanel.js';
 
 
 function SearchArtist(props) {
   const [searchTerm, setSearchTerm] = useState("")
+  const [genres, setGenres] = useState([])
+  // 1900 and 2022 are min and max years, respectively for release year
+  const [minYear, setMinYear] = useState(1900)
+  const [maxYear, setMaxYear] = useState(2022)
+
+  // Function that filters the song genre with the genres array. 
+  // Used because songs can have multiple genres
+  const containsGenre = (songGenre) => {
+    let songGenres = songGenre.split(", ");
+    let filteredGenres = []
+    for (let i = 0; i < songGenres.length; i++) {
+      if (genres.includes(songGenres[i].toLowerCase())) {
+        filteredGenres.push(songGenres[i])
+      }
+    }
+    return filteredGenres.length > 0
+  }
 
   return(
       <div className="SearchArtist">
+        <FilterPanel 
+          className="searchPanel" 
+          genres={genres} 
+          setGenres={setGenres} 
+          minYear={minYear}
+          setMinYear={setMinYear} 
+          maxYear={maxYear} 
+          setMaxYear={setMaxYear}
+        />
         <div className="ScrollView">
           <input
             autoFocus
@@ -18,14 +45,27 @@ function SearchArtist(props) {
             onChange={(e) => setSearchTerm(e.target.value)}
           />
           {songData.filter(
-            (song) => {
-              if(searchTerm === "") {
-                return song
-              }
-              else if(song.artist.toLowerCase().includes(searchTerm.toLowerCase())) {
-                return song;
-              }
+           (song) => {
+            // Use a flag to show if a song does not meet a condition
+            let isShown = true;
+
+            if (genres.length !== 0 && !containsGenre(song.genre)) {
+              isShown = false
             }
+            // comparing int with string works lol
+            if (song.release_year < minYear) { 
+              isShown = false
+            }
+            if (song.release_year > maxYear) {
+              isShown = false;
+            }
+            if(!song.artist.toLowerCase().includes(searchTerm.toLowerCase())) {
+              isShown = false;
+            }
+            if (isShown === true) {
+              return song
+            }
+          }
           ).map(
             (song) => {
               return(
